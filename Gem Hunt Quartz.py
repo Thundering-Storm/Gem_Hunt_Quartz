6a9d61324114c10b239825ef223c8a69954cf9c5
Thundering Storm <thuderingatthestorm@gmail.com>
2025-10-24 17:28:38 +0200

    # game test 3

--- Gem Hunt Quartz.py
+++ Gem Hunt Quartz.py
@@ -1,32 +1,31 @@
-# game test 2 - walls
+# game test 3 - levels
 import pygame
 import math
 
 def ResetVariables():
-    global WindowIsOpen, playerX, playerY, pressedToGoUp, jumpForce, bottomOfGround, wPressed, DEBUGDRAW, wLocked, justJumped
+    global WindowIsOpen, playerX, playerY, jumpForce, DEBUGDRAW, justJumped, Level, touchedEdge, currentLevelWalls
     WindowIsOpen = True
     playerX = displayWidth // 2.5
     playerY = displayHeight // 3
-    pressedToGoUp = 0
-    bottomOfGround = False
-    wPressed = 0
     jumpForce = 0
     DEBUGDRAW = 0
-    wLocked = 0
     justJumped = False
+    Level = 31
+    touchedEdge = False
+    currentLevelWalls = []
     
-def Falling(Mode):
+def Falling(Mode): # gravity and fix downwards movement
     global playerY, jumpForce, downPlayer, PSBe, PSBeY, playerBOX, justJumped
-    downPlayer = pygame.Rect(playerX + 6, playerY + 145, 65, 5)
+    downPlayer = pygame.Rect(playerX + 6, playerY + 146, 65, 5)
     PSBeY = playerY + 6
     PSBe = pygame.Rect(playerX + 6, PSBeY, 65, 145)
     if Mode == 1:
-        return downPlayer.colliderect(ground), downPlayer.colliderect(wall)
+        return downPlayer.colliderect(ground), any(downPlayer.colliderect(wall) for wall in currentLevelWalls)
     
     if Mode == 0:
-        if downPlayer.colliderect(ground) or downPlayer.colliderect(wall):
+        if downPlayer.colliderect(ground) or any(downPlayer.colliderect(wall) for wall in currentLevelWalls):
             if not justJumped:
-                while PSBe.colliderect(ground) or PSBe.colliderect(wall):
+                while PSBe.colliderect(ground) or any(PSBe.colliderect(wall) for wall in currentLevelWalls):
                     PSBeY -= 1
                     PSBe = pygame.Rect(playerX + 6, PSBeY, 65, 150)
                 playerY = PSBeY
@@ -44,42 +43,94 @@ def Falling(Mode):
         pygame.draw.rect(Window, (255, 0, 0), PSBe)
         pygame.draw.rect(Window, (255, 125, 0), downPlayer)
 
-def TouchingWalls(Side):
+def TouchingWalls(Side): # when moving left or right fix movement
     global leftPlayer, rightPlayer, playerX
     LPX = playerX - 300 * Time
     RPX = playerX + 300 * Time
     leftPlayer = pygame.Rect(LPX - 1, playerY + 3, 5, 140)
     rightPlayer = pygame.Rect(RPX + 70, playerY + 3, 5, 140)
     if Side == -1:
-        if not leftPlayer.colliderect(wall):
+        if not any(leftPlayer.colliderect(wall) for wall in currentLevelWalls):
             playerX += math.floor(-300 * Time)
         else:
-            while leftPlayer.colliderect(wall):
+            while any(leftPlayer.colliderect(wall) for wall in currentLevelWalls):
                 LPX += 1
-                leftPlayer = pygame.Rect(LPX + 3, playerY + 5, 75, 150)
+                leftPlayer = pygame.Rect(LPX + 3, playerY + 5, 75, 140)
             playerX = LPX - 1
 
     if Side == 1:
-        if not rightPlayer.colliderect(wall):
+        if not any(rightPlayer.colliderect(wall) for wall in currentLevelWalls):
             playerX += math.ceil(300 * Time)
         else:
-            while rightPlayer.colliderect(wall):
+            while any(rightPlayer.colliderect(wall) for wall in currentLevelWalls):
                 RPX -= 1
-                rightPlayer = pygame.Rect(RPX + 3, playerY + 5, 70, 145)
+                rightPlayer = pygame.Rect(RPX + 3, playerY + 5, 70, 140)
             playerX = RPX + 1
 
     if DEBUGDRAW == 1:
         pygame.draw.rect(Window, (142, 57, 16), leftPlayer)
         pygame.draw.rect(Window, (61, 75, 241), rightPlayer)
 
-def HeadHit():
+def HeadHit(): # if the player hits their head on a wall
     global upPlayer, jumpForce
-    upPlayer = pygame.Rect(playerX + 10, playerY - 4, 65, 5)
-    if upPlayer.colliderect(wall):
+    upPlayer = pygame.Rect(playerX + 6, playerY - 1, 65, 5)
+    if any(upPlayer.colliderect(wall) for wall in currentLevelWalls):
         jumpForce = 0
 
     if DEBUGDRAW == 1:
         pygame.draw.rect(Window, (84, 213, 3), upPlayer)
+
+def LevelEnds(Mode):
+    global playerBOX, touchedEdge, Level, playerX, playerY
+    levelEdge_left = pygame.Rect(0, 0, 1, displayHeight)
+    levelEdge_right = pygame.Rect(displayWidth, 0, 1, displayHeight)
+    levelEdge_up = pygame.Rect(0, 0, displayWidth, 1)
+    levelEdge_down = pygame.Rect(0, displayHeight, displayWidth, 1)
+    
+    if Mode == 0:
+        if not touchedEdge:
+            if playerBOX.colliderect(levelEdge_left):
+                Level -= 10
+                touchedEdge = True
+                playerX = displayWidth - 1
+            if playerBOX.colliderect(levelEdge_right):
+                Level += 10
+                touchedEdge = True
+                playerX = 0
+            if playerBOX.colliderect(levelEdge_up):
+                Level += 1
+                touchedEdge = True
+                playerY = displayHeight - 1
+            if playerBOX.colliderect(levelEdge_down):
+                Level -= 1
+                touchedEdge = True
+                playerY = 0
+        if not any(LevelEnds(1)):
+            touchedEdge = False
+    
+    if Mode == 1:
+        return playerBOX.colliderect(levelEdge_left), playerBOX.colliderect(levelEdge_right), playerBOX.colliderect(levelEdge_up), playerBOX.colliderect(levelEdge_down)
+    
+    if DEBUGDRAW == 1:
+        pygame.draw.rect(Window, (255, 0, 0), levelEdge_left)
+        pygame.draw.rect(Window, (255, 0, 0), levelEdge_right)
+        pygame.draw.rect(Window, (255, 0, 0), levelEdge_up)
+        pygame.draw.rect(Window, (255, 0, 0), levelEdge_down)
+
+def LevelDatabase():
+    global Level, currentLevelWalls
+
+    currentLevelWalls = []
+    
+    if Level == 21: # x1 y1
+        currentLevelWalls = [
+            pygame.Rect(432, 645, 150, 150),
+            pygame.Rect(176, 533, 150, 50),
+            pygame.Rect(432, 389, 150, 50),
+            pygame.Rect(865, 432, 150, 50)
+        ]
+    
+
 
 def PlayerMovement():
     global jumpForce, justJumped
@@ -89,16 +140,9 @@ def PlayerMovement():
     if keyPressed[pygame.K_d]:
         TouchingWalls(1)
     if keyPressed[pygame.K_w] and any(Falling(1)):
-        jumpForce = -600
+        jumpForce = -800
         justJumped = True
 
-def Movement():
-    # down
-    Falling(0)
-    # up
-    HeadHit()
-    # player
-    PlayerMovement()
 
 pygame.init()
 
@@ -108,7 +152,7 @@ displayHeight = info.current_h - 50
 displayWidth = info.current_w
 
 Window = pygame.display.set_mode((displayWidth, displayHeight))
-pygame.display.set_caption("game test 2 - walls")
+pygame.display.set_caption("game test 3 - levels")
 
 ResetVariables()
 
@@ -119,15 +163,22 @@ while WindowIsOpen:
             WindowIsOpen = False
 
     Window.fill((13, 32, 132))
-
+    
     playerBOX = pygame.Rect(playerX, playerY, 75, 150)
     ground = pygame.Rect(0, displayHeight / 1.35, displayWidth, displayHeight)
-    wall = pygame.Rect(452, 425, 150, 150)
 
-    Movement()
+    # Movement
+    Falling(0)
+    HeadHit()
+    PlayerMovement()
 
+    # Levels
+    LevelEnds(0)
+    LevelDatabase()
+    
+    for wall in currentLevelWalls:
+        pygame.draw.rect(Window, (75, 75, 75), wall)
     pygame.draw.rect(Window, (63, 171, 42), ground)
-    pygame.draw.rect(Window, (75, 75, 75), wall)
     pygame.draw.rect(Window, (45, 0, 0), playerBOX)
     
     pygame.display.flip()
